0950a70c3fddc0c3cecf7273a2158a06b6bc325a
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-03-09 11:22:25 -0600

    Confirm modeal and success route for calling dibs

---
+++ src/app/item/[id]/calledDibs/page.tsx
@@ -0,0 +1,11 @@
+const CallDibsPage = () => {
+    return (
+        <div>
+            <h1>Success!</h1>
+            <h2>You Called Dibs</h2>
+            <h4>What happens next?</h4>
+            <p>The owner will reach out to you to setup transfer.</p>
+        </div>
+    )
+}
+export default CallDibsPage;
--- src/app/item/[id]/page.tsx
+++ src/app/item/[id]/page.tsx
@@ -1,19 +1,34 @@
 'use client'
 import DibsButton from "@/components/dibsButton";
+import ItemCard from "@/components/itemCard";
 import { GetItem } from "@/data/client";
 import Item from "@/data/models/item";
 import Image from 'next/image'
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 import { useEffect, useState } from "react";
 
 const ItemPage = () => {
     const [item, setItem] = useState<Item>()
+    const [clickedCallDibs, setClickedCallDibs] = useState(false)
+    const [confirmedCallingDibs, setConfirmedCallingDibs] = useState(false)
     const params = useParams()
     const { id: itemId } = params
 
     const getAndSetItem = async (id: string) => {
         const gottenItem = await GetItem(id)
         setItem(gottenItem)
+    }
+
+    const router = useRouter()
+    const SendToNextPage = (newItemPath: string) => {
+        router.push(newItemPath)
+    }
+
+    const confirmCallingDibs = () => {
+        setConfirmedCallingDibs(true)
+        setClickedCallDibs(false)
+        const nextPagePath = `/item/${item?.$id}/calledDibs`
+        SendToNextPage(nextPagePath)
     }
 
     useEffect(() => {
@@ -39,11 +54,19 @@ const ItemPage = () => {
                     {/* View Q&A */}
 
                     <div className="sticky bottom-0">
-                        <DibsButton item={item} />
+                        <DibsButton item={item} setClickedCallDibs={setClickedCallDibs} />
                     </div>
                 </div>
             ) : (
                     <p>Loading...</p>
+            )}
+            {!!clickedCallDibs && !!item && (
+                <div className="bg-green-900 text-green-100 flex flex-col place-items-center justify-around absolute h-screen w-screen top-0 left-0 z-999">
+                    <h1>Confirm Calling Dibs</h1>
+                    <ItemCard item={item} />
+                    <button className="bg-green-700 ring-2 ring-violet-300 py-4 px-8 rounded-xl" onClick={confirmCallingDibs}>Confirm</button>
+                </div>
+
             )}
             
         </div>
