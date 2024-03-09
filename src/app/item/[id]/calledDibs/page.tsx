144328349e6d59abc3e4bd68894e9bd11de9edf5
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-03-09 16:50:06 -0600

    back to browsing button

--- src/app/item/[id]/calledDibs/page.tsx
+++ src/app/item/[id]/calledDibs/page.tsx
@@ -1,10 +1,18 @@
+'use client'
+import { useRouter } from "next/navigation";
+
 const CallDibsPage = () => {
+    const router = useRouter()
+    const handleClick = () => {
+        router.push('/browse/')
+    }
     return (
         <div>
             <h1>Success!</h1>
             <h2>You Called Dibs</h2>
             <h4>What happens next?</h4>
             <p>The owner will reach out to you to setup transfer.</p>
+            <button className="bg-green-700 ring-2 ring-violet-300 py-4 px-8 rounded-xl" onClick={handleClick}>Back to browsing</button>
         </div>
     )
 }
