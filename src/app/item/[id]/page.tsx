c1d39ae514c097999fca2f22f1d7af8940bd53ed
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-03-12 18:56:44 -0500

    Make call dibs button sticky, make images conditional on having imageUrl, make back button

--- src/app/item/[id]/page.tsx
+++ src/app/item/[id]/page.tsx
@@ -1,6 +1,4 @@
 'use client'
-import DibsButton from "@/components/dibsButton";
-import ItemCard from "@/components/itemCard";
 import { GetItem, UpdateItemIsDibbed } from "@/data/client";
 import Item from "@/data/models/item";
 import Image from 'next/image'
@@ -31,6 +29,10 @@ const ItemPage = () => {
         !!hasUpdated && hasUpdated.isDibbed == true && SendToNextPage(nextPagePath)
     }
 
+    const handleBackButton = () => {
+        setClickedCallDibs(false)
+    }
+
     useEffect(() => {
         getAndSetItem(itemId as string)
     }, [])
@@ -41,13 +43,15 @@ const ItemPage = () => {
                 <div className="flex flex-col">
                     <p>{item.ItemName}</p>
                     <div>
-                        <Image
-                            className="place-self-center w-full md:w-1/2 lg:w-1/3 xl:w-1/4 py-2 rounded-xl aspect-square object-cover"
-                            width={200}
-                            height={200}
-                            src={item.ImageURL}
-                            alt={`User photo of item ${item.ItemName}`}
-                        />
+                        {item.ImageURL && (
+                            <Image
+                                className="place-self-center w-full md:w-1/2 lg:w-1/3 xl:w-1/4 py-2 rounded-xl aspect-square object-cover"
+                                width={200}
+                                height={200}
+                                src={item.ImageURL}
+                                alt={`User photo of item ${item.ItemName}`}
+                            />
+                        )}
                     </div>
                     {/* Image gallery swipe/grid if multiple */}
                     {/* Is owner offering to pay postage */}
@@ -55,27 +59,32 @@ const ItemPage = () => {
                     {/* Ask quesiton / message owner if consent */}
                     {/* View Q&A */}
 
-                    <div className="sticky bottom-0">
-                        <DibsButton item={item} setClickedCallDibs={setClickedCallDibs} />
+                    <div className="absolute w-full bottom-0 left-0 right-0 p-2">
+                        <button onClick={()=>setClickedCallDibs(true)} className="w-full border-2 border-solid border-violet-400 rounded-xl py-4 px-4 bg-green-600 text-violet-900">
+                    I call dibs!
+                        </button>
                     </div>
                 </div>
             ) : (
                     <p>Loading...</p>
             )}
             {!!clickedCallDibs && !!item && (
-                <div className="bg-green-900 text-green-100 flex flex-col place-items-center justify-around absolute h-screen w-screen top-0 left-0 z-999">
+                <div className="bg-green-900 flex flex-col place-items-center justify-center text-lime-200 absolute h-screen w-screen top-0 left-0 z-999">
                     <h1 className="text-3xl">Confirm Calling Dibs</h1>
                     <div className="w-full m-4 flex flex-col items-center space-y-4">
                         <h3 className="text-xl">{item.ItemName}</h3>
-                        <Image
+                        {item.ImageURL && (<Image
                             className="self-center w-fit aspect-square object-cover"
                             width={100}
                             height={100}
                             src={item?.ImageURL}
                             alt={`User photo of item ${item.ItemName}`}
-                        />
+                        />)}
                     </div>
-                    <button className="bg-green-700 ring-2 ring-violet-300 py-4 px-8 rounded-xl" onClick={confirmCallingDibs}>Confirm</button>
+                    <div className="flex gap-4 absolute bottom-0 w-full left-0 right-0 p-4">
+                        <button className="w-1/2 bg-green-700 ring-2 ring-violet-300 py-4 px-8 rounded-xl" onClick={handleBackButton}>Back</button>
+                        <button className="w-1/2 bg-green-700 ring-2 ring-violet-300 py-4 px-8 rounded-xl" onClick={confirmCallingDibs}>Confirm</button>
+                    </div>
                 </div>
 
             )}
--- src/components/itemCard.tsx
+++ src/components/itemCard.tsx
@@ -10,13 +10,15 @@ const ItemCard = ({ item }: Props) => {
     return (
         <Link className="flex flex-col rounded-lg w-full p-2 border border-sold border-green-300 hover:cursor-pointer bg-green-700" href={`/item/${item.$id}/`}>
             <h3 className="text-lg font-semibold">{item.ItemName}</h3>
-            <Image
+            {item.ImageURL && (
+                <Image
                 className="self-center w-full aspect-square object-cover"
                 width={50}
                 height={50}
                 src={item.ImageURL}
                 alt={`User photo of item ${item.ItemName}`}
             />
+            )}
         </Link>
     )
 }
