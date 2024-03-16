a21fb92b79af494ccaf7f16391276a0dc331eb8c
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-03-16 17:20:51 -0500

    Move success page back to browse button to bottom of screen

--- src/app/item/[id]/calledDibs/page.tsx
+++ src/app/item/[id]/calledDibs/page.tsx
@@ -1,3 +1,7 @@
+/**
+ * useRouter only works in Client Components
+ */
+
 'use client'
 import { useRouter } from "next/navigation";
 
@@ -12,7 +16,9 @@ const CallDibsPage = () => {
             <h2>You Called Dibs</h2>
             <h4>What happens next?</h4>
             <p>The owner will reach out to you to setup transfer.</p>
-            <button className="bg-green-700 ring-2 ring-violet-300 py-4 px-8 rounded-xl" onClick={handleClick}>Back to browsing</button>
+            <div className='absolute bottom-0 left-0 right-0 p-2'>
+                <button className="w-full bg-green-700 ring-2 ring-violet-300 py-4 px-8 rounded-xl" onClick={handleClick}>Back to browsing</button>
+            </div>
         </div>
     )
 }
