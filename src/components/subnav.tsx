a235545509fec3f8f682e6348e6a31caccb47747
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-03-23 11:10:27 -0500

    Responsive subheader improvement

--- src/components/subnav.tsx
+++ src/components/subnav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Subnav = () => {
     return (
-        <nav className="bg-green-900 p-2 px-4 pt-14 flex justify-between text-green-400 gap-6">
+        <nav className="bg-green-900 p-2 px-4 pt-14 flex flex-col sm:flex-row md:justify-between text-green-400 gap-6">
             <div className="space-x-8">
                 <Link href='/item/add/'>Add New</Link>
                 <Link href='/browse/'>Browse</Link>
