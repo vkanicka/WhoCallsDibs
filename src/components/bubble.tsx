4c7be0273a047ca64fec36fa614e5ee3b45c86ad
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-04-11 11:32:47 -0500

    Fix bubble position

--- src/components/bubble.tsx
+++ src/components/bubble.tsx
@@ -11,7 +11,7 @@ const Bubble = () => {
         router.push(isBrowsePage ? '/item/add' : '/browse')
     }
     return (
-        <button onClick={handleClick} className="absolute right-6 bottom-32 rounded-full h-12 w-12 bg-verbena-600 z-bubble flex place-content-center shadow-glow shadow-lime-100">
+        <button onClick={handleClick} className="fixed right-6 bottom-32 rounded-full h-12 w-12 bg-verbena-600 z-bubble flex place-content-center shadow-glow shadow-lime-100">
             {isBrowsePage ? <Plus size={30} className='self-center text-limeshine-300' /> : <Search size={30} className='self-center text-limeshine-300' />}
         </button>
     )
