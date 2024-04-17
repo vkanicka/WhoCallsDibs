cd73bbfddb7f45a6d9a61e0dc1090f1c1156f1f4
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-04-17 12:54:59 -0500

    Update email copy

--- src/app/item/[id]/calledDibs/page.tsx
+++ src/app/item/[id]/calledDibs/page.tsx
@@ -15,7 +15,7 @@ const CallDibsPage = () => {
             <h1>Success!</h1>
             <h2>You Called Dibs</h2>
             <h4>What happens next?</h4>
-            <p>The owner will reach out to you to setup transfer.</p>
+            <p>The owner will reach out to you over email to setup transfer.</p>
             <div className='absolute bottom-0 left-0 right-0 p-2'>
                 <button className="btn-v w-full ring-2 ring-violet-300 py-4 px-8 rounded-xl" onClick={handleClick}>Back to browsing</button>
             </div>
