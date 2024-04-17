eb85e603cfe0818692af759bae82ee94437dd945
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-04-17 13:22:53 -0500

    Simplify email copy to prevent truncating

--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -22,8 +22,8 @@ const sendMail = ({ user, item, url }: Props) => {
         from: `Who Called Dibs App <victoria@${process.env.NEXT_PUBLIC_MAILGUN_DOMAIN as string}>`,
         to: [item.itemOwnerEmail, user.email],
         subject: "Dibs Called!",
-        text: `${user.name} Called Dibs on ${item.itemOwnerName}'s Item!nItem dibbed: ${item.ItemName} (url: ${url}). Connect via email. Dibs Caller: ${user.email}, Item Owner: ${item.itemOwnerEmail}.`,
-        html: `<h1>${user.name} Called Dibs on ${item.itemOwnerName}'s Item!</h1><br><h3>Item dibbed: <a href=${url}>${item.ItemName}</a></h3><br><h4>Connect via email:</h4><ul><li>Dibs Caller: ${user.email}</li><li>Item Owner: ${item.itemOwnerEmail}</li></ul>`
+        text: `${user.name} Called Dibs on ${item.itemOwnerName}'s Item - ${item.ItemName} (url: ${url}). Connect via email to setup transfer! Dibs Caller: ${user.email}, Item Owner: ${item.itemOwnerEmail}.`,
+        html: `<h3>${user.name} Called Dibs on ${item.itemOwnerName}'s Item - <a href=${url}>${item.ItemName}</a>.</h3><p>Connect via email to setup transfer! Dibs Caller: ${user.email}, >Item Owner: ${item.itemOwnerEmail}.</p>`
     })
     .then(msg => console.log(msg)) // logs response data
     .catch(err => console.log(err)); // logs any error
