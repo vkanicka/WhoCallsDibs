5ec0dd2cabfce676dcdc4c0498200a61377c8bdc
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-04-20 09:28:59 -0500

    Route from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+'use client'
+
+import { UserContext } from "@/data/context/user";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect } from "react";
+
 export default function Home() {
+  const userCtx = useContext(UserContext)
+  const router = useRouter()
+  
+  const routeUser = () => {
+    if (!!userCtx.user.$id) {
+      router.push(`/browse`)
+    } else {
+      router.push('/account/login')
+    }
+  }
+
+  useEffect(() => {
+    routeUser()
+  }, [userCtx])
+
   return (
     <main>
-      <h1>How This Works</h1>
     </main>
   );
 }
